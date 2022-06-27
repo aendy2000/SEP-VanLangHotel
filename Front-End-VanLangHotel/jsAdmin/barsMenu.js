df7938d3efbca1705afa87a74c267dfbcc9b7c87
Tran Khuong <trankhuong2512001@gmail.com>
2022-06-27 18:36:29 +0700

    Chức năng Admin

---
+++ Front-End-VanLangHotel/jsAdmin/barsMenu.js
@@ -0,0 +1,68 @@
+let btnEditFunc = document.querySelector(".btn-edit__func");
+let btnLock = document.querySelector(".button-lock");
+let btnUnlock = document.querySelector(".button-unlock");
+var iconLock = document.getElementById(".icon-lock")
+// var btnFunc = document.querySelector(".")
+// let searchBtn = document.querySelector(".bx-search");
+
+/*==================== SHOW NAVBAR ====================*/
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+            // change icon
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle','navbar')
+
+/*==================== LINK ACTIVE ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+const navDropdown = document.querySelectorAll(".nav__dropdown");
+for (let i = 0; i < navDropdown.length; i++) {
+  navDropdown[i].addEventListener("click", () =>
+    navDropdown[i].classList.toggle("open")
+  );
+}
+
+
+    btnUnlock.style.display = "none"
+    btnLock.addEventListener('click', function(){
+            btnLock.style.display = "none"
+            btnUnlock.style.display = "inline-block"
+            document.addEventListener('click', function (e) {
+                if (e.target.id === btnEditFunc.id) {
+                    e.preventDefault();
+                }
+            });
+            document.addEventListener('click', function (e){
+                if(e.target.id === iconLock.id){
+                    e.preventDefault();
+                }
+            })
+            
+    })
+
+    btnUnlock.addEventListener('click', function(){
+            btnUnlock.style.display = "none"
+            btnLock.style.display = "inline-block"
+            // document.addEventListener('click', function (e) {
+            //     if (e.target.id !== btnEditFunc.id) {
+            //         e.preventDefault();
+            //     }
+            // });
+    }) 
