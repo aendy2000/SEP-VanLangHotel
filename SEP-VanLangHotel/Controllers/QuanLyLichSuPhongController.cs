f02f0f45bff8ed23a0ce8db2eb520aabbe9c4df7
197PM21928 - TRẦN HUY HOÀNG - K25T-PM1 <hoang.197pm21928@vanlanguni.vn>
2022-08-01 21:25:38 +0700

    Chức Năng Xem danh sách lịch sử thuê phòng

---
+++ SEP-VanLangHotel/Controllers/QuanLyLichSuPhongController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SEP_VanLangHotel.Models;
+using SEP_VanLangHotel.Middleware;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+namespace SEP_VanLangHotel.Controllers
+{
+    [LoginVerification]
+    public class QuanLyLichSuPhongController : Controller
+    {
+        SEP25Team09Entities model = new SEP25Team09Entities();
+        //trang chu lịch sử phòng khách sạn
+        public ActionResult Home()
+        {
+            var ltshistory = model.TT_Dat_Phong.Where(s => s.Trang_Thai == 1).ToList();
+            return View(ltshistory);
+            
+        }
+
+    }
+
+}
